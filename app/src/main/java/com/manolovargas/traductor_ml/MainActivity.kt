865334f345e488560c6407c7984870e768133045
Manolo Vargas <60122495+nova3092@users.noreply.github.com>
2024-03-01 18:49:30 -0500

    Add a languajes

--- app/src/main/java/com/manolovargas/traductor_ml/MainActivity.kt
+++ app/src/main/java/com/manolovargas/traductor_ml/MainActivity.kt
@@ -2,10 +2,81 @@ package com.manolovargas.traductor_ml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.manolovargas.traductor_ml.Modelo.Idioma
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  Et_Idioma_Origen: EditText
+    private lateinit var  Tv_Idioma_Destino: EditText
+    private lateinit var  Btn_Elegir_Idioma: MaterialButton
+    private lateinit var  Btn_Idioma_Elegido: MaterialButton
+    private lateinit var  Btn_Traducir: MaterialButton
+
+
+    private var  IdiomaArrayList: ArrayList<Idioma>? = null
+
+    companion object {
+        private const val REGISTRO = "Mis_registros"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        InicializarVistas()
+        IdiomasDisponibles()
+
+        Btn_Idioma_Elegido.setOnClickListener {
+            Toast.makeText(this, "Idioma elegido", Toast.LENGTH_SHORT).show()
+        }
+
+        Btn_Idioma_Elegido.setOnClickListener {
+            Toast.makeText(this, "Idioma elegido", Toast.LENGTH_SHORT).show()
+        }
+
+        Btn_Traducir.setOnClickListener {
+            Toast.makeText(this, "Traducido", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun InicializarVistas(){
+
+        Et_Idioma_Origen = findViewById(R.id.Et_Idioma_Origen)
+        Tv_Idioma_Destino = findViewById(R.id.Tv_Idioma_Destino)
+        Btn_Elegir_Idioma = findViewById(R.id.Btn_Elegir_Idioma)
+        Btn_Idioma_Elegido = findViewById(R.id.Btn_Idioma_Elegido)
+        Btn_Traducir = findViewById(R.id.Btn_Traducir)
+
+    }
+
+    private fun IdiomasDisponibles(){
+
+        IdiomaArrayList=ArrayList<Idioma>()
+
+        val ListaCodigoIdioma= TranslateLanguage.getAllLanguages()
+
+        //es-español
+
+        for (codigo_lenguaje in ListaCodigoIdioma){
+
+            val titulo_lenguaje= Locale(codigo_lenguaje).displayLanguage
+
+            Log.d(REGISTRO, "IdiomasDisponibles: codigo_lenguaje $titulo_lenguaje")
+            Log.d(REGISTRO, "IdiomasDisponibles: codigo_lenguaje $codigo_lenguaje")
+
+
+
+
+            val modeloIdioma= Idioma(codigo_lenguaje,titulo_lenguaje)
+
+            IdiomaArrayList!!.add(modeloIdioma)
+        }
+
     }
 }
